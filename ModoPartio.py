a1af148bba7afbb77b6762acfa60ebdfde3ce205
Ben Rogall <brogall@alum.mit.edu>
2013-05-29 11:22:15 -0500

    added mac to kit config. Switched to fileSave dialog type in script

--- ModoPartio.py
+++ ModoPartio.py
@@ -14,7 +14,7 @@ def main():
         args = lx.args()
 
         if args[0] == "SelectFile":
-            lx.command( 'dialog.setup',   style = 'fileOpen' )
+            lx.command( 'dialog.setup',   style = 'fileSave' )
             lx.command( 'dialog.title', title='Select Cache File' )
             lx.command( 'dialog.fileTypeCustom', format='icecache', username='Softimage ICECACHE', loadPattern="*.icecache;", saveExtension="icecache" )
             lx.command( 'dialog.fileTypeCustom', format='bin', username='Realflow BIN', loadPattern="*.bin", saveExtension="bin" )
