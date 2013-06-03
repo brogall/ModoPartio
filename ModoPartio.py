d1c14905ae8e2bc57d662ae717c73949a95a1dd7
Ben Rogall <brogall@alum.mit.edu>
2013-06-03 10:26:24 -0500

    Changed script to use fileOpen vs fileSave depending on partioMode.

--- ModoPartio.py
+++ ModoPartio.py
@@ -9,26 +9,36 @@ def double_quote(in_string):
     out_string = '"' + in_string + '"'
     return out_string
 
+def SelectFile():
+    partioMode = lx.eval("item.channel partioMode ?")
+    if partioMode == 2:         #   particle caching mode
+        lx.command( 'dialog.setup',   style = 'fileSave' )
+    else:                       #   reading cache
+        lx.command( 'dialog.setup',   style = 'fileOpen' )
+    lx.command( 'dialog.title', title='Select Cache File' )
+    lx.command( 'dialog.fileTypeCustom', format='icecache', username='Softimage ICECACHE', loadPattern="*.icecache;", saveExtension="icecache" )
+    lx.command( 'dialog.fileTypeCustom', format='bin', username='Realflow BIN', loadPattern="*.bin", saveExtension="bin" )
+    lx.command( 'dialog.fileTypeCustom', format='prt', username='Krakatoa PRT', loadPattern="*.prt", saveExtension="prt" )
+    lx.command( 'dialog.fileTypeCustom', format='bgeo', username='Houdini BGEO', loadPattern="*.bgeo", saveExtension="bgeo" )     
+    lx.command( 'dialog.fileTypeCustom', format='pdc', username='Maya PDC', loadPattern="*.pdc", saveExtension="pdc" )   
+    lx.command( 'dialog.fileTypeCustom', format='pda', username='Maya PDA', loadPattern="*.pda", saveExtension="pda" )  
+    lx.command( 'dialog.fileTypeCustom', format='pda', username='Maya PDA', loadPattern="*.pda", saveExtension="pda" )
+    try:                                         
+        lx.command( 'dialog.open')    
+        cache_path =  lx.eval(  'dialog.result ?' )
+        cache_path = path.abspath(cache_path)
+        return cache_path
+    except:
+        return None
+
 def main():
     try:
         args = lx.args()
 
         if args[0] == "SelectFile":
-            lx.command( 'dialog.setup',   style = 'fileSave' )
-            lx.command( 'dialog.title', title='Select Cache File' )
-            lx.command( 'dialog.fileTypeCustom', format='icecache', username='Softimage ICECACHE', loadPattern="*.icecache;", saveExtension="icecache" )
-            lx.command( 'dialog.fileTypeCustom', format='bin', username='Realflow BIN', loadPattern="*.bin", saveExtension="bin" )
-            lx.command( 'dialog.fileTypeCustom', format='prt', username='Krakatoa PRT', loadPattern="*.prt", saveExtension="prt" )
-            lx.command( 'dialog.fileTypeCustom', format='bgeo', username='Houdini BGEO', loadPattern="*.bgeo", saveExtension="bgeo" )     
-            lx.command( 'dialog.fileTypeCustom', format='pdc', username='Maya PDC', loadPattern="*.pdc", saveExtension="pdc" )   
-            lx.command( 'dialog.fileTypeCustom', format='pda', username='Maya PDA', loadPattern="*.pda", saveExtension="pda" )  
-            lx.command( 'dialog.fileTypeCustom', format='pda', username='Maya PDA', loadPattern="*.pda", saveExtension="pda" )                                         
-            lx.command( 'dialog.open')    
-            cache_path =  lx.eval(  'dialog.result ?' )
-            cache_path = path.abspath(cache_path)
-
-
-            lx.eval('item.channel cacheFileName ' + double_quote(cache_path))
+            cache_path = SelectFile()
+            if(cache_path != None):
+                lx.eval('item.channel cacheFileName ' + double_quote(cache_path))
 
 
     except:
